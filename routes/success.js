e9fb361d39656bd78d68bfdf1fecdfdae1d3c1eb
bcbell <coleman.britt@gmail.com>
2020-12-29 03:51:12 -0500

    Updated: Success and cancel pages

--- routes/success.js
+++ routes/success.js
@@ -4,7 +4,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.render('artist/success', {title: ''});
+  res.render('artist/success', {title: 'Thank you for your order!'});
 });
 
 module.exports = router;
